bb9c422edaa52e293676c714b3c136c1405f7992
Arinerron <arinesau@gmail.com>
2017-09-25 23:11:27 -0700

    bash

---
+++ run.bat
@@ -0,0 +1,6 @@
+CD src
+
+javac -cp .:../\*:../lib/\* com/arinerron/utils/internetometerbot/InternetometerBotMain.java
+java -cp .:../\*:../lib/\* com/arinerron/utils/internetometerbot/InternetometerBotMain $@
+
+PAUSE
